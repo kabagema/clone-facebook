292d38e3f07c4a18bc08fbd23f1acdeea3d7e84e
kabagema <35719135+kabagema@users.noreply.github.com>
2020-08-30 11:24:53 +0200

    logo

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   //Bem naming convention
   return (
     <div className="app">
       <h1> Facebook clone</h1>
-          
+      <Header />
+      
       {/* Header */}
       {/* App Body*/}
         {/* Sidebar Feed */}
--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -4,9 +4,22 @@ function Header() {
     return (
         <div className = "header">
             {/* Center Left Right */}
-            
+            <div className="header__left">
+                <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1365px-Facebook_f_logo_%282019%29.svg.png"
+                        alt=""
+                        />
+            </div>             
+            <div className="header__middle">
+                
+            </div>
+            <div className="header__right">
+                
+            </div>
         </div>
     )
 }
 
 export default Header
+
+
