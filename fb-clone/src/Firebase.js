c052bebf94f3e153f045389e90aa720f70280bcd
kabagema <35719135+kabagema@users.noreply.github.com>
2020-08-30 11:07:39 +0200

    config

---
+++ fb-clone/src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDWGhjOuaFD_o8G5ICPdNkA13sdu0nWsio",
+    authDomain: "kabagema-fb-clone.firebaseapp.com",
+    databaseURL: "https://kabagema-fb-clone.firebaseio.com",
+    projectId: "kabagema-fb-clone",
+    storageBucket: "kabagema-fb-clone.appspot.com",
+    messagingSenderId: "875640431931",
+    appId: "1:875640431931:web:042254d7aff7dfd706431e"
+  };
