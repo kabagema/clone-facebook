8724ba6d8b281f6a68239407a8b999c3d2cd4335
kabagema <35719135+kabagema@users.noreply.github.com>
2020-08-31 01:42:06 +0200

    sidebar

--- fb-clone/src/Sidebar.js
+++ fb-clone/src/Sidebar.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import './sidebar.css'
 import SidebarRow from './SidebarRow'
-
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import chatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import ExpandMoreOutlined from "@material-ui/icons/ExpandMoreOutlined";
 function Sibebar() {
     return (
         <div className = "sidebar">
         
-            <SidebarRow title="Pages"/>
-            <SidebarRow title="Friends"/>
-            <SidebarRow />
+            <SidebarRow src={user.photoURL} title={user.displayName} />
+            <SidebarRow 
+                Icon={localHospitalIcon}
+                title="Friends"
+            />
+
+            <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+            <SidebarRow Icon={PeopleIcon} title="Friends" />
+            <SidebarRow Icon={chatIcon} title="Messenger" />
+            <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+            <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
         </div>
     )
 }
 
 export default Sibebar
+
+// https://youtu.be/B-kxUMHBxNo?t=4868
--- fb-clone/src/SidebarRow.js
+++ fb-clone/src/SidebarRow.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Avatar, Icon} from "@material-ui/core"
 import './SidebarRow.css'
 function SidebarRow({src, icon, title}) {
     return (
         <div className="SidebarRow">
-            <p>{title}</p>
+            {src && <Avatar src={src} />}
+            {Icon && <Icon />}
+
+            <h4>{title}</h4>
             <p>{icon}</p>
             <p>{title}</p>
         </div>
