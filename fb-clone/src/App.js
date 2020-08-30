ffa1a68ada7f2db14057649040e2c6ab2ee5a9ad
kabagema <35719135+kabagema@users.noreply.github.com>
2020-08-30 11:15:23 +0200

    Header

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -6,6 +6,12 @@ function App() {
   return (
     <div className="app">
       <h1> Facebook clone</h1>
+          
+      {/* Header */}
+      {/* App Body*/}
+        {/* Sidebar Feed */}
+        {/* Widgets */}
+
     </div>
   );
 }
---
+++ fb-clone/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className = "header">
+            {/* Center Left Right */}
+            
+        </div>
+    )
+}
+
+export default Header
